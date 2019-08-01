fe645f71a2ee4f55b0bb872d336d9b7f0bd40b36
carlos.hudson <carlos.hudson@hp.com>
2019-08-01 11:52:54 -0500

    59. Implementing Authentication 04:43

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { User } from './user.model';
+import { AuthData } from './auth-data.model';
+
+export class AuthService {
+    private user: User;
+
+    registerUser(authData: AuthData) {
+        this.user = {
+            email: authData.email,
+            userId: Math.round(Math.random() * 10000).toString()
+        };
+    }
+}
