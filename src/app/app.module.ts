3c15c6389237ef332167b73c426dad39fb874505
carlos.hudson <carlos.hudson@hp.com>
2019-08-02 14:48:45 -0500

    63. Injecting Using the Training Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //Customed Material Module
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +19,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { StopTrainingComponent } from './training/current-training/stop-training.component';
+import { AuthService } from './auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -40,9 +41,10 @@ import { StopTrainingComponent } from './training/current-training/stop-training
     BrowserAnimationsModule, 
     MaterialModule,
     FlexLayoutModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule 
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent],
   //If the compponent is not being rendered by a selector or by routing (instaciated)
   entryComponents: [StopTrainingComponent] //This is because this component is being rendered by another component
--- src/app/auth/auth-data.model.ts
+++ src/app/auth/auth-data.model.ts
@@ -1,4 +1,4 @@
 export interface AuthData {
     email: string;
-    passord: string;
+    password: string;
 }
